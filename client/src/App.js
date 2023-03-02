4a12b121b52a9852768161f83e1d07314cb8fc41
Nick DiMuzio <NDiMuzio@gscommunitycare.org>
2023-03-02 14:58:09 -0500

    Built Front End Container

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
+import react from "react";
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+    display: flex;
+`;
+
+const Main = styled.div`
+    flex: 7;
+`;
+
+const Wrapper = styled.div``;
+
 function App() {
-  return (
-    <div>
-      Hello world!
-    </div>
-  );
+    return (
+        <Container>
+            <Menu />
+            <Main>
+                <Navbar />
+                <Wrapper>Video Cards</Wrapper>
+            </Main>
+        </Container>
+    );
 }
 
 export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import react from "react";
+import styled from "styled-components"
+
+const Container = styled.div`
+
+`
+
+const Wrapper = styled.div`
+
+`
+
+const Navbar = () => {
+ return (
+    <Container>
+        Navbar
+    </Container>
+ )   
+}
+
+export default Navbar
