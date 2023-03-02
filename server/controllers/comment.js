af370c5ce3c203a846a108babba1c9accfe4d84f
Nick <ndd243@gmail.com>
2023-03-01 19:13:31 -0500

    Add Comments Routes

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,3 +1,37 @@
-export const comment = (req,res) => {
-    res.json("JSON from comment controller")
-}
+import { createError } from "../error.js";
+import Comment from "../models/comment.js";
+import Video from "../models/video.js";
+
+export const addComment = async (req, res, next) => {
+  const newComment = new Comment({ ...req.body, userId: req.user.id });
+  try {
+    const savedComment = await newComment.save();
+    res.status(200).json(savedComment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+    const video = await Video.findById(comment.videoId)
+    if (comment.userId === req.user.id || video.userId === req.user.id) {
+        await Comment.findByIdAndDelete(req.params.id)
+        res.status(200).json("Comment has been deleted.")
+    } else {
+        return next(createError(403,"Can only delete your own comment or a comment on your own video."))
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({videoId:req.params.videoId})
+    res.status(200).json(comments)
+  } catch (err) {
+    next(err);
+  }
+};
