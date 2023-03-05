e542d471a6145d9c881f87549330e6ed15646893
Nick <ndd243@gmail.com>
2023-03-04 23:11:40 -0500

    Finish Sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,103 @@
-import react from "react";
-import styled from "styled-components";
+import react, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/theme.js";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 
 const Container = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 const Main = styled.div`
-    flex: 7;
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text};
 `;
 
 const Wrapper = styled.div``;
 
 function App() {
-    return (
-        <Container>
-            <Menu />
-            <Main>
-                <Navbar />
-                <Wrapper>Video Cards</Wrapper>
-            </Main>
-        </Container>
-    );
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Main>
+          <Navbar />
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+          <Wrapper>Video Cards</Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,39 +1,169 @@
 import react from "react";
 import styled from "styled-components";
 import LogoImg from "../img/birds.png";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import HistoryIcon from "@mui/icons-material/History";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import SportsFootballIcon from "@mui/icons-material/SportsFootball";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import MovieIcon from "@mui/icons-material/Movie";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ReportIcon from "@mui/icons-material/Report";
+import HelpIcon from "@mui/icons-material/Help";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
 
 const Container = styled.div`
-    flex: 1;
-    background-color: #202020;
-    height: 100vh;
-    color: white;
+  flex: 1;
+  background-color: ${({ theme }) => theme.bg};
+  height: 100vh;
+  color: ${({ theme }) => theme.text};
+  position: sticky;
+  top: 0;
+  overflow-y: scroll;
 `;
 
 const Wrapper = styled.div`
-    padding: 18px 26px;
+  padding: 18px 26px;
 `;
 
 const Logo = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 5px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
 `;
 
 const Img = styled.img`
-    height: 25px;
+  height: 25px;
 `;
 
-const Menu = () => {
-    return (
-        <Container>
-            <Wrapper>
-                <Logo> 
-                    <Img src={LogoImg} />
-                    BirdTube
-                </Logo>
-            </Wrapper>
-        </Container>
-    );
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0px;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  display: flex;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Login = styled.div``;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+const Title = styled.h2`
+    font-size: 14px;
+    font-weight: 500;
+    color: #aaaaaa;
+    margin-bottom: 20px;
+`
+
+const Menu = ({darkMode,setDarkMode,theme}) => {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={LogoImg} />
+          BirdTube
+        </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsIcon />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Item>
+          <VideoLibraryIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryIcon />
+          History
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comment, and subscribe.
+          <Button>
+            <AccountCircleOutlinedIcon />
+            SIGN IN
+          </Button>
+        </Login>
+        <Hr />
+        <Title>BEST OF BIRDTUBE</Title>
+        <Item>
+          <LibraryMusicIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsFootballIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsIcon />
+          Games
+        </Item>
+        <Item>
+          <MovieIcon />
+          Movies
+        </Item>
+        <Item>
+          <NewspaperIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvIcon />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsIcon />
+          Settings
+        </Item>
+        <Item>
+          <ReportIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpIcon />
+          Help
+        </Item>
+        <Item onClick={() => setDarkMode(!darkMode)}>
+          <SettingsBrightnessIcon />
+          Theme
+        </Item>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Menu;
